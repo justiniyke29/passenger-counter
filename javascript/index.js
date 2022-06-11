9c4cf31016495f5ab0c80df65880fd5539d8f252
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-06-11 22:47:14 +0100

    new addition
    added a save function

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,11 @@
 let countEl = document.getElementById("count-el")
-console.log(countEl)
+/* console.log(countEl) */
 let count=0
 function increment(){
     count= count +1
     countEl.innerText=count
+}
+function save(){
+console.log(count)
 }
 
